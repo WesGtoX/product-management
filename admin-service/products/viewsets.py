f2fb52646c2b41f2bc5deecc8873b5f394a784e7
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2022-08-14 04:04:48 -0300

    feat(admin-service): adding publish methods with correct data

--- admin-service/products/viewsets.py
+++ admin-service/products/viewsets.py
@@ -4,6 +4,7 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from producer import publish
 from products.models import Product, User
 from products.serializers import ProductSerializer
 
@@ -19,6 +20,7 @@ class ProductViewSet(viewsets.ViewSet):
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+        publish('product_created', serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk=None):
@@ -31,17 +33,27 @@ class ProductViewSet(viewsets.ViewSet):
         serializer = ProductSerializer(instance=product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+        publish('product_updated', serializer.data)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def destroy(self, request, pk=None):
         product = Product.objects.get(pk=pk)
         product.delete()
+        publish('product_deleted', dict(pk=pk))
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class UserAPIView(APIView):
 
+    def post(self, response):
+        user = User.objects.create()
+        return Response(dict(id=user.id, message='User registered successfully'))
+
     def get(self, response):
         users = User.objects.all()
-        user = random.choice(users)
-        return Response(dict(id=user.id))
+
+        if users.count() > 0:
+            user = random.choice(users)
+            return Response(dict(id=user.id))
+
+        return Response(dict(message='No registered user'))

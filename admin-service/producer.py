7fe7425c9179347ee975b29e7de449e61c197716
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2022-08-14 04:03:55 -0300

    feat(admin-service): update producer

--- admin-service/producer.py
+++ admin-service/producer.py
@@ -1,15 +1,17 @@
+import json
+
 import pika
-from django.conf import settings
+from decouple import config
 
 # params = pika.URLParameters(url=settings.BROKER_URL)
 params = pika.URLParameters(url='amqps://zripliyj:jfDsB3Ia_uu6QAstDE3L1IMZqYQ4uBjd@jackal.rmq.cloudamqp.com/zripliyj')
 
-connection = pika.BlockingConnection(params)
 
+params = pika.URLParameters(url=config('BROKER_URL', default=''))
+connection = pika.BlockingConnection(params)
 channel = connection.channel()
 
 
-def publish(method='', body=''):
-    # producer = 'admin'
-    producer = 'main'
-    channel.basic_publish(exchange='', routing_key=producer, body=f'hello world {producer}')
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='main', body=json.dumps(body).encode(), properties=properties)

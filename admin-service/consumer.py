ccc2efeea94150839ff262c78fcd2986d820f866
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2022-08-14 04:06:38 -0300

    feat(admin-service): update consumer to count likes

--- admin-service/consumer.py
+++ admin-service/consumer.py
@@ -1,24 +1,32 @@
+import json
+import os
+
+import django
 import pika
-from django.conf import settings
+from decouple import config
 
-params = pika.URLParameters(url=settings.BROKER_URL)
 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'admin.settings'
+
+django.setup()
+
+from products.models import Product
+
+params = pika.URLParameters(url=config('BROKER_URL', default=''))
 connection = pika.BlockingConnection(params)
-
 channel = connection.channel()
-
-
 channel.queue_declare(queue='admin')
 
+
 def callback(ch, method, properties, body):
-    print('Receive in admin')
-    print(body)
+    data = json.loads(body)
+
+    product = Product.objects.get(pk=data.get('pk'))
+    product.likes += 1
+    product.save()
 
 
 channel.basic_consume(queue='admin', on_message_callback=callback, auto_ack=True)
 
-print('Started Consuming')
-
 channel.start_consuming()
-
 channel.close()

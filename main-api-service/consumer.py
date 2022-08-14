388274951f0e5208372de97c1735a3d5c9e2cff9
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2022-08-14 04:12:31 -0300

    feat(main-api-service): consumer update with service

--- main-api-service/consumer.py
+++ main-api-service/consumer.py
@@ -1,24 +1,38 @@
+import json
+import logging
+import os
+
 import pika
-from main import app
+from service import APIService
 
-params = pika.URLParameters(url=app.config.get('BROKER_URL'))
+logger = logging.getLogger(__name__)
 
+
+params = pika.URLParameters(url=os.getenv('BROKER_URL', default=''))
 connection = pika.BlockingConnection(params)
-
 channel = connection.channel()
-
 channel.queue_declare(queue='main')
 
 
 def callback(ch, method, properties, body):
-    print('Receive in main')
-    print(body)
+    logger.info('Receive in main')
+    data = json.loads(body)
+
+    service = APIService(data)
+
+    if properties.content_type == 'product_created':
+        service.create()
+
+    if properties.content_type == 'product_updated':
+        service.update()
+
+    if properties.content_type == 'product_deleted':
+        service.delete()
 
 
 channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
 
-print('Started Consuming')
+logger.info('Started Consuming')
 
 channel.start_consuming()
-
 channel.close()
